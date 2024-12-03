392dcfa10b7a13807ce4408543b881155806d058
Caleb Wolthers <w0473807@nscc.ca>
2024-12-03 17:58:21 -0400

    A few enemy sprites, and changes to Enemy

--- Assets/Scripts/Enemy Scripts/Slime.cs
+++ Assets/Scripts/Enemy Scripts/Slime.cs
@@ -79,6 +79,12 @@ public class Slime : Enemy
         }
     }
 
+
+    private void DoDash()
+    {
+
+    }
+
     private void StartWindUp()
     {
         combatState = SlimeCombatState.WindUp;
